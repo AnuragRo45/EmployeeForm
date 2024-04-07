0dddee65ace3386db8ac1b9e3c8d386f01aca88b
Anurag <anragro@gmail.com>
2024-04-07 23:50:40 +0530

    EmployeeForm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DataGridComponent } from './data-grid/data-grid.component';
+import { AddDataComponent } from './add-data/add-data.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataGridComponent,
+    AddDataComponent
   ],
   imports: [
     BrowserModule,
